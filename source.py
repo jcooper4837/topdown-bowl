9025bce2778bdd35b68a0965be4b570f225f7767
Cooper Martin <jcooper4837@gmail.com>
2020-09-16 00:12:25 -0600

    added more ball functionality including angle and meter

--- source.py
+++ source.py
@@ -27,6 +27,7 @@ class SportsBall:
         self.w.create_rectangle(300, 575, 350, 25, fill="#2b3030")
         self.b = self.w.create_oval(175, 575, 225, 525, fill="red")
         self.l = self.w.create_line(200, 550, 200, 525, fill="black")
+        self.m = self.w.create_line(375, 550, 375, 550, fill="black")
 
         def select(event):
             self.state += 1
@@ -52,15 +53,30 @@ class SportsBall:
             elif self.state == 2:
                 time.sleep(0.025)
                 c = self.changeAngle()
+                r = self.getRotation(c)
+                self.w.coords(self.l, self.w.coords(self.l)[0], self.w.coords(self.l)[1], r[0][0], r[1][0])
                 self.w.update()
             elif self.state == 3:
                 time.sleep(0.025)
-                c = self.changeSpeed()
-                self.w.move(self.b, 0, c)
-                self.w.move(self.l, 0, c)
+                m = self.changeSpeed()
+                self.w.coords(self.m, self.w.coords(self.m)[0], self.w.coords(self.m)[1], self.w.coords(self.m)[0], self.w.coords(self.m)[1]+m-100)
                 self.w.update()
             elif self.state == 4:
                 self.state = 0
+
+    def getRotation(self, ch):
+        cos = math.cos(ch/100)
+        sin = math.sin(ch/100)
+        a = [[cos, -sin, -(self.w.coords(self.l)[0])*cos+(self.w.coords(self.l)[1])*sin+(self.w.coords(self.l)[0])],
+        [sin, cos, -(self.w.coords(self.l)[0])*sin-(self.w.coords(self.l)[1])*cos+(self.w.coords(self.l)[1])],
+        [0, 0, 1]]
+        b = [[self.w.coords(self.l)[2]], [self.w.coords(self.l)[3]], [1]]
+        r = [[0], [0], [0]]
+        for i in range(len(a)):
+            for j in range(len(b[0])):
+                for k in range(len(b)):
+                    r[i][j] += a[i][k] * b[k][j]
+        return r
 
     def changePosition(self):
         t = self.meter
@@ -91,7 +107,6 @@ class SportsBall:
         return self.meter - t
 
     def changeSpeed(self):
-        t = self.meter
         if self.increase:
             if self.meter < 100:
                 self.meter += 10
@@ -102,6 +117,6 @@ class SportsBall:
                 self.meter -= 10
             else:
                 self.increase = True
-        return self.meter - t
+        return self.meter
 
 SportsBall()
